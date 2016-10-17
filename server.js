d904432bc69986247039fdc089c5aa335fc9e8d5
eix07 <rivera-miguel@hotmail.com>
2016-10-16 21:24:14 -0500

    Primer push pag web

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan= require('morgan');
+
+var app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('*',function(req,res){
+	res.sendFile(__dirname+'/public/views/index.html');
+});
+
+app.listen(3000,function(err){
+	if(err){
+		console.log(err);
+	}else{
+		console.log("Listening on port 3000!")
+	}
+});
+
