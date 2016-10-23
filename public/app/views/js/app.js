1d304dd6836a41be825ded47bb6a12a4ec921e1b
eix07 <rivera-miguel@hotmail.com>
2016-10-23 17:36:33 -0500

    Envio de mensajes a nuestros correos por angular y otras correcciones de bugs

---
+++ public/app/views/js/app.js
@@ -0,0 +1,5 @@
+angular.module('myapp',[]).controller('mainController',function($scope,$http){
+	$scope.send=function(mail){
+		$http.post('/api/send',{subject:mail.subject,email:mail.email});
+	}
+});
--- public/controller.js
+++
@@ -1,8 +0,0 @@
-angular.module("controller",[])
-
-	.controller('controlhorarios',function(Sch){
-		var vm=this;
-		vm.show().succes(function(data){
-			vm=data
-		});
-})
