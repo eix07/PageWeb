f32e4c19d30f57ccc8359fc91b8729bd19ae9ee1
eix07 <rivera-miguel@hotmail.com>
2016-10-23 14:46:47 -0500

    Update

--- public/app.js
+++ public/app.js
@@ -1,1 +1,16 @@
-angular.module('myapp',['service','service1','controller','controller1'])
+angular.module('MyApp').controller('Main',function($scope,$http){
+
+	$scope.postData={};
+
+	$scope.postMail=function(contact){
+		if($scope.contactForm.$invalid==true){
+			return
+		}
+	$scope.postData=angular.copy(contact);
+
+	$http.post('/',$scope.postData)
+	.success()
+
+    };
+
+});
